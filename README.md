3a9ac83c7d3c7ddfcd5c49590966e56c95f9afae
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-24 23:45:59 -0600

    added screenshots to my vs

--- README.md
+++ README.md
@@ -15,6 +15,236 @@ SVG Tutorial: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial
 Generate shapes: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
 Applying text in logo's: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts
 logo.svg: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Getting_Started
+SVG Link: https://marketplace.visualstudio.com/items?itemName=jock.svg
+
+# generate-a-svg-logo
+
+
+
+# "12 Random Facts about the country your movie was filed in"<br> Interactive Front-End Application 
+
+
+
+## Technology Used 
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+What is SVG: https://en.wikipedia.org/wiki/SVG
+SVG Tutorial: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial
+Generate shapes: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
+Applying text in logo's: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts
+logo.svg: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Getting_Started
+
+
+
+## Description 
+This application is a Node.js command-line app that takes the users input to generate a logo and save it as a .svg file. The application prompts the user to seleect a color and shaepe, then prompts them to input the text for desired logo, and finally ave the generated SVG to a .svg file. This in turn will keep the user from having to pay a graphic designer to create a simple logo for their projects.
+
+## Visual image of our Deplloyed site
+
+![Alt text](./assets/images/Screen%20Shot%202023-04-14%20at%201.48.45%20PM.png)
+
+
+
+
+
+## Code examples
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ ```sh
+
+ <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+
+<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="GREEN"/>
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="WHITE">SVG</text>
+
+</svg>
+
+
+```
+
+**(ABOVE)- This element fetches via its xlink:href. This is the code that generates the image.
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+```sh
+    
+ 
+
+```
+
+**(ABOVE)- 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+```sh
+
+
+
+```
+**(ABOVE)- 
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+```sh
+  
+
+
+```
+**(ABOVE)- 
+
+```sh
+![Alt text](./assets/images/screen-capture%20(2).webm)
+
+```
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## A GIF of our page load showing the animation of our page loading and a search resulting in a list of 12 random facts.
+
+
+[screen-capture (2).webm](https://user-images.githubusercontent.com/128196644/232180341-b63a7117-5b80-4e32-8342-dc7435e1f979.webm)
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+## Author Info
+
+### Amanda Gray
+
+* [Linkedin]()
+* [Deployed Application link]()
+```
+
+## Credits
+
+Shout out to all the TA's and Google Search!
+
+GOOGLE!  Seriously, thank you google search!
+
+
+
+
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+
+
+
+
+
+
+# generate-a-svg-logo
+
+
+
+# "12 Random Facts about the country your movie was filed in"<br> Interactive Front-End Application 
+
+
+
+## Technology Used 
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+What is SVG: https://en.wikipedia.org/wiki/SVG
+SVG Tutorial: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial
+Generate shapes: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
+Applying text in logo's: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts
+logo.svg: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Getting_Started
+
+
+
+## Description 
+This application is a Node.js command-line app that takes the users input to generate a logo and save it as a .svg file. The application prompts the user to seleect a color and shaepe, then prompts them to input the text for desired logo, and finally ave the generated SVG to a .svg file. This in turn will keep the user from having to pay a graphic designer to create a simple logo for their projects.
+
+## Visual image of our Deplloyed site
+
+![Alt text](./assets/images/Screen%20Shot%202023-04-14%20at%201.48.45%20PM.png)
+
+
+
+
+
+## Code examples
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ ```sh
+
+ <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+
+<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="GREEN"/>
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="WHITE">SVG</text>
+
+</svg>
+
+
+```
+
+**(ABOVE)- This element fetches via its xlink:href. This is the code that generates the image.
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+```sh
+    
+ 
+
+```
+
+**(ABOVE)- 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+```sh
+
+
+
+```
+**(ABOVE)- 
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+```sh
+  
+
+
+```
+**(ABOVE)- 
+
+```sh
+![Alt text](./assets/images/screen-capture%20(2).webm)
+
+```
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## A GIF of our page load showing the animation of our page loading and a search resulting in a list of 12 random facts.
+
+
+[screen-capture (2).webm](https://user-images.githubusercontent.com/128196644/232180341-b63a7117-5b80-4e32-8342-dc7435e1f979.webm)
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+## Author Info
+
+### Amanda Gray
+
+* [Linkedin]()
+* [Deployed Application link]()
+```
+
+## Credits
+
+Shout out to all the TA's and Google Search!
+
+GOOGLE!  Seriously, thank you google search!
+
+
+
+
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,122 +1,129 @@
-const filesystem = require('./node_modules/graceful-fs/graceful-fs')
-const inquirer = require('inquirer');
-const {
-    Circle,
-    square,
-    Triange
-} = require('./lib/shapes');
 
 //Below defines a svg class that has a constructor with 3 methods for rendering and setting the shape and text elements in the svg string.
-class Svg {
-    constructor() {
+const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
+
+// Defines a Svg class that has a constructor with 3 methods for rendering and setting the text/shape elements in the SVG string.
+class Svg{
+    constructor(){
         this.textElement = ''
         this.shapeElement = ''
-
     }
-    render() {
+    render(){
+
         return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
     }
-    setTextElement(text, color) {
+    setTextElement(text,color){
         this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
     }
-    setShapeElement(shape) {
+    setShapeElement(shape){
         this.shapeElement = shape.render()
-    }
-}
 
-//I defined the array of questions using the 'inquirer' library with questions
-const questions = [{
+    }
+    
+}
+//Here are the prompt questions for the user to answer to create their logo in the comand line 
+const questions = [
+    {
         type: "input",
-        name: 'text',
-        message: 'Text: Enter up to three characters:',
+        name: "text",
+        message: "TEXT: Enter up to (3) Characters:",
     },
     {
         type: "input",
-        name: 'text-color',
-        message: 'Text color: Enter a color keyword/or a hexadecimal number:',
+        name: "text-color",
+        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
     },
     {
         type: "input",
-        name: 'shape',
-        message: 'Shape color: Enter a color keyword/or a hexadecimal number:',
+        name: "shape",
+        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
     },
     {
         type: "list",
-        name: 'texpixel-image',
-        message: 'Pick which Pixel Image you would like',
-        Choices: ['Circle', 'Square', 'Triangle'],
+        name: "pixel-image",
+        message: "Choose which Pixel Image you would like?",
+        choices: ["Circle", "Square", "Triangle"],
     },
 ];
 
-
-//Below is the function I created to write the data to file
+// Function to write data to file
 function writeToFile(fileName, data) {
-    console.log('Writting [' + data + '] to file [' + fileName + ']')
-    filesystem.writeFile(fileName, data, function (error) {
-        if (error) {
-            return console.log(error);
+	console.log("Writing [" + data + "] to file [" + fileName + "]")
+    filesystem.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
         }
-        console.log('YES! you have generated your personal logo!');
+        console.log("Congratulations, you have Generated a logo.svg!");
     });
 }
 
-
 async function init() {
-    console.log('Starting init');
-    var svgString = '';
-    var svg_file = 'logo.svg';
+    console.log("Starting init");
+	var svgString = "";
+	var svg_file = "logo.svg";
 
-    //This is the prommpt for the user to answer the questions
+    // Prompt the user for answers
     const answers = await inquirer.prompt(questions);
 
-    //User can answer 
-    var user_text = '';
-    if (answers.text.length > 0 && answers.text.length < 4) {
-        //letting user know only 1-3 char.
-        user_text = answers.text;
-    } else {
-        //if the user inputs more than 4, it will let them know it is invalid
-        console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less please");
+	//user inputs text
+	var user_text = "";
+	if (answers.text.length > 0 && answers.text.length < 4) {
+		user_text = answers.text;
+	} else {
+		// no more than chars. or invalid entry
+		console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
         return;
-    }
-    console.log('User text: [' + user_text + ']');
-    //font color
-    user_font_color = answers['text-color'];
-    console.log('User font color: [' + user_font_color + ']');
-    //shape color
-    user_shape_color = answers.shape;
-    console.log("User shape color: [" + user_shape_color + "]");
-    //shape type
-    user_shape_type = answers["pixel-image"];
-    console.log("User entered shape = [" + user_shape_type + "]");
+	};
+	
+	console.log("User text: [" + user_text + "]");
+	//user font color
+	user_font_color = answers["text-color"];
+	console.log("User font color: [" + user_font_color + "]");
+	//user shape color
+	user_shape_color = answers.shape;
+	console.log("User shape color: [" + user_shape_color + "]");
+	//user shape type
+	user_shape_type = answers["pixel-image"];
+	console.log("User entered shape = [" + user_shape_type + "]");
+	
+	//user shape
+	let user_shape;
+	if (user_shape_type === "Square" || user_shape_type === "square") {
+		user_shape = new Square();
+		console.log("User selected Square shape");
+	}
+	else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+		user_shape = new Circle();
+		console.log("User selected Circle shape");
+	}
+	else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+		user_shape = new Triangle();
+		console.log("User selected Triangle shape");
+	}
+	else {
+		console.log("Invalid shape!");
+	}
+	user_shape.setColor(user_shape_color);
 
+	// Create a new Svg instance and add the shape and text elements to it
+	var svg = new Svg();
+	svg.setTextElement(user_text, user_font_color);
+	svg.setShapeElement(user_shape);
+	svgString = svg.render();
+	
+	//Print shape to log
+	console.log("Displaying shape:\n\n" + svgString);
 
-    //These are the user shape functions
-    let user_shape;
-    if (user_shape_type === 'Square' || user_shape_type === "square") {
-        user_shape = new square();
-        console.log('User selected the Square shape');
-    } else if (user_shape_type === 'Circle' || user_shape_type === 'circle') {
-        user_shape = new Circle();
-        console.log('User selected the Circle shape');
-    } else if (user_shape_type === 'Triangle' || user_shape_type === 'triangle') {
-        user_shape = new Triangle();
-        console.log('User selected the Triangle shape');
-    } else {
-        console.log('Invalid Shape!');
-    }
-    user_shapr.setCOlor(user_shape_color);
-
-    //Below will create a new svg instance and add shape and text to logo
-    var svg = new Svg();
-    svg.setTextElement(user_text, user_font_color);
-    svg.setShapeElement(user_shape);
-    svgString = svg.render();
-
-    //create and display logo!
-    console.log('Displaying shape:\n\n' + svgString);
-    console.log("Shape generation complete!");
-    console.log("Writing shape to file...");
-    writeToFile(svg_file, svgString);
+	console.log("Shape generation complete!");
+	console.log("Writing shape to file...");
+	writeToFile(svg_file, svgString); 
 }
-init()
+init();
+
+
+
+
+
+
